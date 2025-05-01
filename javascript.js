04e936488aeb01339d30823f1941836ad266eeeb
ffrndzTech <199731008+ffernandezTech@users.noreply.github.com>
2025-05-01 02:24:19 -0700

    fixed: adding class to div

--- javascript.js
+++ javascript.js
@@ -5,7 +5,6 @@ const getMainDiv = document.querySelector('#main-container');
 for(let i=1; i<=256;i++)
 {
     const createDiv = document.createElement('div');
-    createDiv.className.add('.box');
-    createDiv.style.backgroundColor='black';
+    createDiv.classList.add('box')
     getMainDiv.appendChild(createDiv);
 }
