9bcddeb73957e06104eec0b998ae57894539dcd6
ffrndzTech <199731008+ffernandezTech@users.noreply.github.com>
2025-05-01 02:16:16 -0700

    feat: still fixing mistake

--- javascript.js
+++ javascript.js
@@ -5,6 +5,7 @@ const getMainDiv = document.querySelector('#main-container');
 for(let i=1; i<=256;i++)
 {
     const createDiv = document.createElement('div');
-    createDiv.classList.add('.box');
+    createDiv.className.add('.box');
+    createDiv.style.backgroundColor='black';
     getMainDiv.appendChild(createDiv);
 }
