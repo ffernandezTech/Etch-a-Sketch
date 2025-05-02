8db89e7eeb20682d5ffdcc317e66c87749b8e2f1
ffrndzTech <199731008+ffernandezTech@users.noreply.github.com>
2025-05-02 00:44:29 -0700

    feat: created function for random color

--- javascript.js
+++ javascript.js
@@ -23,14 +23,27 @@ const getDivs = document.querySelectorAll('.box');
 
 // console.log(getDivs);
 
+function createRGB()
+{
+    return Math.floor(Math.random() * (255-0))
+}
+
+for(let x=0 ; x<=255; x++)console.log(createRGB());
+
+
+
+
 getDivs.forEach(item=>{
 
     item.addEventListener('mouseover', ()=>{
 
         // console.log(item);
-        item.classList.add('box_active');
+
+        item.style.backgroundColor = `rgb(${createRGB()},${createRGB()},${createRGB()} )`;
+        //.classList.add('box_active');
         setTimeout(() => {
-           item.classList.remove('box_active');
+           //item.classList.remove('box_active');
+           item.style.backgroundColor = 'white'
         }, 1000);
 
 
