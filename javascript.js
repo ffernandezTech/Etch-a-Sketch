0d364bfbdfdca01790c6ad1f029ac82a27e5895e
ffrndzTech <199731008+ffernandezTech@users.noreply.github.com>
2025-05-02 00:49:59 -0700

    feat: added comments

--- javascript.js
+++ javascript.js
@@ -2,13 +2,14 @@ const getMainDiv = document.querySelector('#main-container');
 
 
 
-for(let i=1; i<=256;i++)
-{
-    const createDiv = document.createElement('div');
-    createDiv.classList.add('box')
-    getMainDiv.appendChild(createDiv);
-}
+// for(let i=1; i<=256;i++)
+// {
+//     const createDiv = document.createElement('div');
+//     createDiv.classList.add('box')
+//     getMainDiv.appendChild(createDiv);
+// }
 
+//
 function createBoxes(){
 
     for(let i=1; i<=256;i++)
@@ -19,10 +20,8 @@ function createBoxes(){
         }  
 }
 
-const getDivs = document.querySelectorAll('.box');
 
-// console.log(getDivs);
-
+//functoin to return a random number between 0-255 for our RGB
 function createRGB()
 {
     return Math.floor(Math.random() * (255-0))
@@ -31,16 +30,20 @@ function createRGB()
 for(let x=0 ; x<=255; x++)console.log(createRGB());
 
 
+//getting all the divs with class box, and setting an addEventListener for each
+const getDivs = document.querySelectorAll('.box');
 
-
+// console.log(getDivs);
 getDivs.forEach(item=>{
 
     item.addEventListener('mouseover', ()=>{
 
         // console.log(item);
-
+        //css for background color calling a function to get the number of values
         item.style.backgroundColor = `rgb(${createRGB()},${createRGB()},${createRGB()} )`;
         //.classList.add('box_active');
+
+        //using setTimeout to change it to white or it resets
         setTimeout(() => {
            //item.classList.remove('box_active');
            item.style.backgroundColor = 'white'
